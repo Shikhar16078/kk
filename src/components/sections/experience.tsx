25e9ff2715124a7326394bf78d7f73f0afa08c27
Shikhar Kumar <skuma154@ucr.edu>
2025-09-14 23:21:15 +0000

    now it looks good, but building image is colliding with vertical line

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -14,10 +14,10 @@ export function Experience() {
     <SectionWrapper id="experience">
       <SectionTitle>Work Experience</SectionTitle>
       <div className="relative flex flex-col gap-y-12">
-        <div className="absolute left-6 top-0 h-full w-0.5 bg-border"></div>
+        <div className="absolute left-3 top-0 h-full w-0.5 bg-border"></div>
         {workExperience.map((job, index) => (
-          <div key={index} className="relative pl-16">
-            <div className="absolute -left-1.5 top-1.5 flex h-10 w-10 items-center justify-center rounded-full border-2 border-primary bg-background">
+          <div key={index} className="relative pl-12">
+            <div className="absolute -left-5 top-1 flex h-10 w-10 items-center justify-center rounded-full border-2 border-primary bg-background">
               <Building className="h-5 w-5 text-primary" />
             </div>
             <Card>
