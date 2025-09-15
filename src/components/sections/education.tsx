7bdd789d3b06c6d11776aa453c8aa74ac828f614
Shikhar Kumar <skuma154@ucr.edu>
2025-09-15 04:12:06 +0000

    why is ggsipu image smaller?

--- src/components/sections/education.tsx
+++ src/components/sections/education.tsx
@@ -38,7 +38,7 @@ export function Education() {
                         alt={`${edu.institution} logo`}
                         width={40}
                         height={40}
-                        className="rounded-full object-contain"
+                        className="rounded-full object-cover"
                       />
                     </div>
                     <div>
