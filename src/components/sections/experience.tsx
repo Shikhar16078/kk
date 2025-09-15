bde677433e9c0451820fa15303b96987f455357d
Shikhar Kumar <skuma154@ucr.edu>
2025-09-15 06:06:34 +0000

    why is not the vertial line till the end of the last card? also move tim

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -22,7 +22,7 @@ export function Experience() {
     <SectionWrapper id="experience">
       <SectionTitle>Work Experience</SectionTitle>
       <div className="relative flex flex-col gap-y-12">
-        <div className="absolute left-3 top-5 -z-10 h-[calc(100%-4rem)] w-0.5 bg-border md:left-5"></div>
+        <div className="absolute left-3 top-5 -z-10 h-[calc(100%-2rem)] w-0.5 bg-border md:left-5"></div>
         {workExperience.map((job, index) => (
           <div key={index} className="relative pl-12 md:pl-16">
             <div className="absolute -left-2 top-3 flex h-10 w-10 items-center justify-center rounded-full border-2 border-primary bg-background p-2 md:left-0">
