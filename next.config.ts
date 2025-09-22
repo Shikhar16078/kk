bdd4dea49f58d05722e7115e476e48b859ded597
Shikhar Kumar <shikharkumar78@gmail.com>
2025-09-22 20:33:10 +0000

    now images are showing up correctly but css is broken :(

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type {NextConfig} from 'next';
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isGithubPages ? '/kk' : undefined,
   images: {
     unoptimized: true,
   },
